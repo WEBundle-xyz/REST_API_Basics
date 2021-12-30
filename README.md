fc55806cd9436b2a02f711fd24ec67f9ad975739
WEBundleXYZ <stefg@webundlexyzsmbp.mynetworksettings.com>
2021-12-30 08:13:02 -0500

    Created the express.js server

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 ### REST API - Basics
 
-[1 - REST API Basics - Project setup ](../../tree//)
+[1 - REST API Basics - Project setup ](../../tree/1423b310543d111ea26076b844e1a8bd7318ce22/)
 
 [2 - Empty ](../../tree//)
 
--- routes/feed.js
+++ routes/feed.js
@@ -1,8 +1,10 @@
 const express = require('express');
 
+const feedController = require('../controllers/feed');
+
 const router = express.Router();
 
-router.get('/posts');
+router.get('/posts', feedController.getPosts);
 
 module.exports = router;
 
